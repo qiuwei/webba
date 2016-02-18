9458e87b252538af1be9f7863277e061c8107291
Wei Qiu <wei.qiu@uni-tuebingen.de>
2016-02-19 00:01:51 +0100

    Work from home

--- src/main/frontEnd/src/actions/action.js
+++ src/main/frontEnd/src/actions/action.js
@@ -3,14 +3,22 @@ import _ from 'lodash';
 import bows from 'bows';
 import reduxCrud from 'redux-crud';
 import cuid from 'cuid';
+import hyperagent from 'hyperagent';
 
 import actionTypes from './actionTypes';
+
+
 const baseActionCreators = reduxCrud.actionCreatorsFor('webba');
 const log = bows('webba-actions');
 
 
 let actionCreators = {
     fetch() {
+        return function(dispatch, getState) {
+        const action = baseActionCreators.fetchStart();
+            dispatch(action);
+            const rooturl = '/api';
+        };
 
     },
 
